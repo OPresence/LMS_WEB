23568c19522c87cbbeefb6c643bc5ed22daa8098
vikashvishwakarma1601 <vishwakarma.vikash1601@gmail.com>
2024-01-21 04:10:08 +0530

    master-final :  - Fix problemasdooo

--- scripts/commit-msg-hook.sh
+++ scripts/commit-msg-hook.sh
@@ -9,5 +9,5 @@ BRANCH_NAME=$(git branch | grep '*' | sed 's/* //')
 # Check if the branch name contains an underscore
 if [ "$(echo "$BRANCH_NAME" | grep -c '_')" -gt 0 ]; then
   # Prepend the branch name as a prefix to the commit message
-  sed -i.bak -e "1s/^/$PREFIX: /" "$1"
+  sed -i.bak -e "1s/^/\[$PREFIX\]: /" "$1"
 fi
